c9f060acc154dfe2af21f2b73860ef56ff8f27de
Taina Townsend <40957192+tainatownsend@users.noreply.github.com>
2026-08-26 18:31:07 -0700

    feat(app): add north star canonical routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import SavedPassagesPage from '@/pages/SavedPassagesPage'
 import PassagePage from '@/pages/PassagePage'
 import DiscoverPage from '@/pages/DiscoverPage'
 import GettingStartedPage from '@/pages/GettingStartedPage'
+import ReflectionPage from '@/pages/ReflectionPage'
+import FavoritesPage from '@/pages/FavoritesPage'
+import CommunityPage from '@/pages/CommunityPage'
 import EvolutionPage from '@/pages/EvolutionPage'
 import SettingsPage from '@/pages/SettingsPage'
 import { getAppFontSize, getThemeColor } from '@/features/ui/displayPreferences'
@@ -48,45 +51,19 @@ export default function App() {
           <Route path="/criar-conta" element={user ? <Navigate to="/home" replace /> : <AuthPage initialMode="signup" />} />
           <Route path="/redefinir-senha" element={<ResetPasswordPage />} />
 
-          <Route path="/home" element={
-            <ProtectedRoute><HomePage /></ProtectedRoute>
-          } />
-
-          <Route path="/comecar" element={
-            <ProtectedRoute><GettingStartedPage /></ProtectedRoute>
-          } />
-
-          <Route path="/descobrir" element={
-            <ProtectedRoute><DiscoverPage /></ProtectedRoute>
-          } />
-
-          <Route path="/trecho/:sectionId" element={
-            <ProtectedRoute><PassagePage /></ProtectedRoute>
-          } />
-
-          <Route path="/livro/:id" element={
-            <ProtectedRoute><BookDetailPage /></ProtectedRoute>
-          } />
-
-          <Route path="/ler/:id" element={
-            <ProtectedRoute><ReaderPage /></ProtectedRoute>
-          } />
-
-          <Route path="/biblioteca" element={
-            <ProtectedRoute><LibraryPage /></ProtectedRoute>
-          } />
-
-          <Route path="/salvos" element={
-            <ProtectedRoute><SavedPassagesPage /></ProtectedRoute>
-          } />
-
-          <Route path="/evolucao" element={
-            <ProtectedRoute><EvolutionPage /></ProtectedRoute>
-          } />
-
-          <Route path="/configuracoes" element={
-            <ProtectedRoute><SettingsPage /></ProtectedRoute>
-          } />
+          <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path="/comecar" element={<ProtectedRoute><GettingStartedPage /></ProtectedRoute>} />
+          <Route path="/descobrir" element={<ProtectedRoute><DiscoverPage /></ProtectedRoute>} />
+          <Route path="/trecho/:sectionId" element={<ProtectedRoute><PassagePage /></ProtectedRoute>} />
+          <Route path="/livro/:id" element={<ProtectedRoute><BookDetailPage /></ProtectedRoute>} />
+          <Route path="/ler/:id" element={<ProtectedRoute><ReaderPage /></ProtectedRoute>} />
+          <Route path="/biblioteca" element={<ProtectedRoute><LibraryPage /></ProtectedRoute>} />
+          <Route path="/reflexoes" element={<ProtectedRoute><ReflectionPage /></ProtectedRoute>} />
+          <Route path="/favoritos" element={<ProtectedRoute><FavoritesPage /></ProtectedRoute>} />
+          <Route path="/comunidade" element={<ProtectedRoute><CommunityPage /></ProtectedRoute>} />
+          <Route path="/salvos" element={<ProtectedRoute><SavedPassagesPage /></ProtectedRoute>} />
+          <Route path="/evolucao" element={<ProtectedRoute><EvolutionPage /></ProtectedRoute>} />
+          <Route path="/configuracoes" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
